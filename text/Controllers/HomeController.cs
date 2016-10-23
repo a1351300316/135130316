e22bcd9bfaadff70773aa6d06ea331bc2b00860e
1351300315 <2013abc>
2016-10-23 14:27:02 +0800

    添加新闻

---
+++ text/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace text.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult News()
+        {
+            //ViewBage
+            ViewBag.message = "hello";
+            ViewBag.Date = DateTime.Now;
+
+
+            //viewdata
+            ViewData["message"] = "hello";
+            ViewData["Date"] = DateTime.Now;
+
+            string[] news = new string[]
+            {
+
+                "四道口附近哈老公看了",
+                "收到了饭卡里说的法律是",
+            };
+
+            ViewBag.News = news;
+            ViewData["News"] = news;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ text/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(text.Startup))]
+namespace text
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

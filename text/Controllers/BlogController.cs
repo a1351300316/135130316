55cf8efc6177aa321e6c0c64e3f35ed14f13da29
stu <stu@qq.com>
2016-11-02 11:46:54 +0800

    sousuo

--- text/Controllers/BlogController.cs
+++ text/Controllers/BlogController.cs
@@ -3,83 +3,98 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using text;
 
-namespace text.Controllers
+namespace News.Controllers
 {
     public class BlogController : Controller
     {
         // GET: Blog
-        public ActionResult Index()
+        public ActionResult Index(string q)
         {
             var db = new BlogDatabase();
-            
+
             db.Database.CreateIfNotExists();
-            
-            var lst = db.BlogArticles.OrderByDescending(o => o.Id).ToList();
-            ViewBag.BlogArticles = lst;
+
+            var lst = db.BlogArticles.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(q))
+            {
+                lst = lst.Where(o => o.Subject.Contains(q));
+            }
+
+
+            ViewBag.BlogArticles = lst.OrderByDescending(o => o.Id).ToList();
+            ViewBag.q = q;
+
             return View();
         }
 
+
         public ActionResult AddArticle()
         {
-           return View();
+            return View();
         }
- 
+
         public ActionResult ArticleSave(string subject, string body)
         {
-             var article = new BlogArticle();
-             article.Subject = subject;
-             article.Body = body;
-             article.DateCreated = DateTime.Now;
- 
-             var db = new BlogDatabase();
+            var article = new BlogArticle();
+            article.Subject = subject;
+            article.Body = body;
+            article.DateCreated = DateTime.Now;
+
+            var db = new BlogDatabase();
             db.BlogArticles.Add(article);
-             db.SaveChanges();
- 
-             return Redirect("Index");
-         }
- 
+            db.SaveChanges();
+
+            return Redirect("Index");
+        }
+
         public ActionResult Show(int id)
-         {
-             var db = new BlogDatabase();
-             var article = db.BlogArticles.First(o => o.Id == id);
- 
-             ViewData.Model = article;
-             return View();
-
-         }
-        public ActionResult Edit(int id)
-         {
-             var db = new BlogDatabase();
-             var article = db.BlogArticles.First(o => o.Id == id);
- 
-             ViewData.Model = article;
-             return View();
-         }
- 
-         public ActionResult EditSave(int id, string subject, string body)
-         {
-             var db = new BlogDatabase();
-             var article = db.BlogArticles.First(o => o.Id == id);
- 
-             article.Subject = subject;
-             article.Body = body;
-
-             db.SaveChanges();
- 
-             return RedirectToAction("Index");
-         }
-        public ActionResult Delete(int id)
-         {
-             var db = new BlogDatabase();
+        {
+            var db = new BlogDatabase();
             var article = db.BlogArticles.First(o => o.Id == id);
- 
-             db.BlogArticles.Remove(article);
-             db.SaveChanges();
- 
-             return RedirectToAction("Index");
-         }
-}
- } 
 
-   
+            ViewData.Model = article;
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var db = new BlogDatabase();
+            var article = db.BlogArticles.First(o => o.Id == id);
+
+            ViewData.Model = article;
+            return View();
+        }
+
+        public ActionResult EditSave(int id, string subject, string body)
+        {
+            var db = new BlogDatabase();
+            var article = db.BlogArticles.First(o => o.Id == id);
+
+            article.Subject = subject;
+            article.Body = body;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// 删除博文
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Delete(int id)
+        {
+            var db = new BlogDatabase();
+            var article = db.BlogArticles.First(o => o.Id == id);
+
+            db.BlogArticles.Remove(article);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}

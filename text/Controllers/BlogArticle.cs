dcb11984c2c31260a337110a30b9f3f5c42476d2
stu <stu@qq.com>
2016-11-07 17:41:48 +0800

    改

---
+++ text/Controllers/BlogArticle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace News.Controllers
+{
+    internal class BlogArticle
+    {
+        public BlogArticle()
+        {
+        }
+
+        public object Body { get; internal set; }
+        public DateTime DateCreated { get; internal set; }
+        public object Subject { get; internal set; }
+    }
+}
--- text/Controllers/BlogController.cs
+++ text/Controllers/BlogController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using text;
 
-namespace News.Controllers
+namespace text.Controllers
 {
     public class BlogController : Controller
     {
